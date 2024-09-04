6347bcaa228ddf08ce48de6263ff96db6b94b6ee
Julian During <duringjulian@gmail.com>
2024-09-04 15:44:28 +0200

    Add links to gpx file

--- R/tdf_overview_tbl.R
+++ R/tdf_overview_tbl.R
@@ -1,11 +1,19 @@
 tdf_overview_tbl <- function(sf_tdf_stages) {
-  sf_tdf_stages |>
+  df_stages <- sf_tdf_stages |>
     as_tibble() |>
-    select(where(negate(is_list))) |>
+    select(where(negate(is_list)))
+  
+  df_stages |>
+    select(-c(x, start_finish)) |>
     reactable(
       groupBy = c("edition", "type"), selection = "multiple", filterable = TRUE,
       defaultSelected = seq_len(nrow(sf_tdf_stages)),
       columns = list(
+        gpx_path = colDef(cell = function(value, index) {
+          htmltools::tags$a(
+            href = value, target = "_blank",
+            as.character(df_stages[index, "start_finish"]))
+        }),
         type = colDef(aggregate = "frequency"),
         km = colDef(aggregate = "sum", format = colFormat(digits = 2))))
 }
